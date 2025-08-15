02a3276fbfdbfffd69f0c558271d8bbe48179f17
Al3J077 <garciaalejo711@gmail.com>
2025-08-15 19:44:14 +0000

    prueba_mejorar

---
+++ prueba.js
@@ -0,0 +1,35 @@
+//2. Cuadrado de un numero 
+function cuadradoNumero(numero) {
+    let cuadrado = numero * numero;
+
+    console.log ("el cuadrado es: ", cuadrado);
+}
+
+//5. Numeros consecutivos con un ciclo
+function numeroConsecutivo(numero) {
+    for(var numero=0; numero>=7; numero++) {
+    console.log(numero)
+}  
+}
+
+
+
+//8. Objeto con nombre, edad y ciudad, mostrando propiedad en consola
+
+var Persona= {
+    nombre:"Alejandro",
+    edad:21,
+    ciudad:"Bogota"
+}
+console.log(Persona.nombre, Persona.edad, Persona.ciudad)
+
+
+//7. Crear un array con 10 números y mostrar solo los que sean mayores al numero ingresado como parametro
+
+
+function numeroParametro() {}
+
+var diezNumero=[3,5,7,9,2,3,12,23,11,16]
+
+
+
